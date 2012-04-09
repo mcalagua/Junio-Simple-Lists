12b0c33f318ff015d4dc1062866e3b76af857927
mcalagua <mcalaguap@gmail.com>
2012-04-09 02:19:49 -0500

    creacion de la estructura de la kata

---
+++ mcalagua/simple_lists.rb
@@ -0,0 +1,7 @@
+class SingleLinkedNode
+  attr_accessor :sig, :value
+
+  def initialize(val)
+    self.value = val
+  end
+end
---
+++ mcalagua/simple_lists_spec.rb
@@ -0,0 +1,7 @@
+require './simple_lists.rb'
+
+shared_examples "a list" do
+
+
+end
+
